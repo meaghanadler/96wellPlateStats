2c8ba8c11dba7065329aee9873fe00a99281bce5
meaghanadler <madler5@vols.utk.edu>
2023-12-12 11:19:05 -0500

    edits

--- R/badwell.R
+++ R/badwell.R
@@ -2,15 +2,16 @@
 #'
 #' @param df A dataframe.
 #' @param well A column header
+#' @param key A dataframe
 #' @returns A dataframe.
 #' @export
 
-badwell<- function(df, well){
+badwell<- function(df, well,key){
   dfgoodwellz <- df |>
     select(-c({{well}}))|>
     pivot_longer(!time,names_to="Well",values_to="OD600")|>
     mutate(Repgroup = str_extract(Well, "[aA-zZ]{1,1}"))|>
     group_by(Repgroup, time)|>
     summarize(Mean = mean(OD600), Std = sd(OD600))|>
-    left_join(idkey, by = "Repgroup")
+    left_join(key, by = "Repgroup")
 }
--- R/platemeanstd.R
+++ R/platemeanstd.R
@@ -1,14 +1,15 @@
 #' Calculating the Mean and standard deviation between replicates
 #'
 #' @param df A dataframe.
+#' @param key A dataframe
 #' @returns A dataframe.
 #' @export
 
-platemeanstd <- function(df){
+platemeanstd <- function(df,key){
   dfstats <- df |>
     pivot_longer(!time,names_to="Well",values_to="OD600")|>
     mutate(Repgroup = str_extract(Well, "[aA-zZ]{1,1}"))|>
     group_by(Repgroup, time)|>
     summarize(Mean = mean(OD600), Std = sd(OD600))|>
-    left_join(idkey, by = "Repgroup")
+    left_join(key, by = "Repgroup")
 }
